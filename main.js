e431a0fec4b6026cf4bc23174c0dc3a75e2bfd6b
thulioph <thulioph@gmail.com>
2017-08-18 20:23:11 -0300

    Creates a menubar instead of window.

--- main.js
+++ main.js
@@ -66,8 +66,8 @@ function createMenubar() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-// app.on('ready', createMenubar);
+// app.on('ready', createWindow)
+app.on('ready', createMenubar);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
